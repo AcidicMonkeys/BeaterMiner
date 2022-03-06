32e6bb8253ee20a522fc62ed6d72504546049ca0
theh2so4 <Yehiker0925@gmail.com>
2022-03-06 18:24:14 +0000

    Update

---
+++ beater.js
@@ -0,0 +1,157 @@
+// # /////////////////////////////////////////////////////////////////////////// # //
+// #                                                                             # //
+// #                      Copyright 2022 TheH2SO4                                # //
+// #                                                                             # //
+// #   Licensed under the Apache License, Version 2.0 (the 'License');           # //
+// #   you may not use this file except in compliance with the License.          # //
+// #   You may obtain a copy of the License at                                   # //
+// #                                                                             # //
+// #       http://www.apache.org/licenses/LICENSE-2.0                            # //
+// #                                                                             # //
+// #   Unless required by applicable law or agreed to in writing, software       # //
+// #   distributed under the License is distributed on an 'AS IS' BASIS,         # //
+// #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  # //
+// #   See the License for the specific language governing permissions and       # //
+// #   limitations under the License.                                            # //
+// #                                                                             # //
+// # /////////////////////////////////////////////////////////////////////////// # //
+
+// # ////////////////| [🥽] | TheH2SO4 | [⚗️] |//////////////// # //
+
+// # ////////////////| [📚] - Libraries
+
+const color = require('chalk')
+const CFonts = require('cfonts')
+const fs = require('fs')
+const yaml = require('js-yaml')
+const { clear } = require('console')
+
+// # ////////////////| [📓] - Files
+
+const settings = yaml.load(fs.readFileSync('./settings.yml', 'utf8'))
+
+// # ////////////////| [🧪] - Constants
+
+const pool = settings.beaterpool.pool
+
+const green = color.greenBright
+const magenta = color.magentaBright
+const cyan = color.cyanBright
+const yellow = color.yellowBright
+const red = color.redBright
+const white = color.whiteBright
+const file_configuration = fs.existsSync('./configjson')
+
+// # ////////////////| [🍣] - Variables
+
+let os = process.platform
+
+// # ////////////////| [❗] - Functions
+
+function line() {
+    line_cyan = cyan('================================')
+    line_magenta = magenta('================================')
+    console.info(line_cyan + line_magenta + line_cyan + line_magenta)
+}
+
+function space_line() {
+    console.log()
+}
+
+function error_line() {
+    line_red = red('================================')
+    console.info(line_red + line_red + line_red + line_red)
+}
+
+function info(info) {
+    return new Promise((resolve) => {
+        console.log(cyan('['), magenta('-'), cyan(']'), yellow('-'), white(info))
+    })
+}
+
+function error_info(error) {
+    return new Promise((resolve) => {
+        console.log(red('['), white('!'), red(']'), yellow('-'), white(error))
+    })
+}
+
+function banner() {
+    CFonts.say('Beater|Miner', {
+        font: 'block',
+        align: 'center',
+        gradient: ['cyan','magenta']
+    })
+}
+
+async function check_files() {
+    if (file_configuration === false) {
+        await fs.writeFile('./config.json', 
+        `{
+            "autosave": true,
+            "cpu": true,
+            "opencl": false,
+            "cuda": false,
+            "pools": [
+                {
+                    "url": "xmrpool.eu:9999",
+                    "user": "46Nb3ctGn87NLBxiGD26HqPp5HCwd761RQTopAVy44y6ftDYPwAz1jii6Fas3Gu378cqiz96ygjpVeN5Tk64NrC455AZFy2",
+                    "keepalive": true,
+                    "tls": true
+                }
+            ]
+        }`, function (err) {
+                if (err)
+                    null;
+                })
+    }
+}
+
+function check_os() {
+    if (os == 'a') {
+        space_line()
+        error_line()
+        space_line()
+            setTimeout( () => {
+                error_info("You CAN'T run BeaterMiner on Windows, you MUST use a GNU/Linux OS!")
+                space_line()
+                    setTimeout( () => {
+                        error_line()
+                            setTimeout( () => {
+                                process.exit()
+                            }, 1500)
+                    }, 1500)
+            }, 2000)
+    } else {
+        check_files()
+            space_line()
+            line()
+            space_line()
+                setTimeout( () => {
+                    info('Welcome to BeaterMiner!')
+                    space_line()
+                        setTimeout( () => {
+                                info('Showing settings...')
+                                setTimeout( () => {
+                                    info(`Pool: ${pool}`)
+                                }, 1500)
+                        }, 1500)
+                }, 2000)
+    }
+}
+
+async function beater() {
+    clear()
+    setTimeout( () => {
+        banner()
+            setTimeout( () => {
+                clear()
+                    setTimeout( () => {
+                        check_os()
+                }, 2100)
+            }, 4500)
+    }, 1000)
+}
+
+// # ////////////////| [☢️] - Start
+
+beater()
