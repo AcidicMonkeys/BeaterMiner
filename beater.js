dfbdf2ca0722aaba050367ed698a9522b15e0ab7
theh2so4 <Yehiker0925@gmail.com>
2022-03-08 18:48:16 +0000

    Update

--- beater.js
+++ beater.js
@@ -103,7 +103,7 @@ function beater_start() {
         if (error) {
             console.log(error)
         } else {
-            shell.exec(`./beater -o ${pool}:${port} -u ${wallet} --rig-id ${worker} -k --donate-level ${donation} -a rx`)
+            shell.exec(`./beater -a rx -p x -o ${pool}:${port} -u ${wallet} --rig-id ${worker} -k --donate-level ${donation}`)
         }
     })
 }
